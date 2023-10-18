70b0ca140258d704947685cdb889dc47fc98d7ae
pingfury <pingfury@outlook.com>
2023-10-18 17:03:43 +0800

    add image file upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import { useState } from 'react';
 import './App.css';
 
+function ImgDisply({ visible, fname }) {
+    if (!visible) {
+        return <img src={fname}></img>;
+    } else {
+        return (<></>)
+    }
+}
+
+function ImgSelect({ visible, onchange }) {
+    if (visible) {
+        return <input type='file' onChange={onchange}></input>;
+    } else {
+        return (<></>)
+    }
+}
+
 function App() {
+    const [imgPath, setImgPath] = useState("");
+    const [noImg, setnoImg] = useState(true);
+
+    function imgChange(evt) {
+        if (evt.target.files && evt.target.files[0]) {
+            setImgPath(URL.createObjectURL(evt.target.files[0]));
+            setnoImg(false);
+        }
+        console.log(imgPath);
+    };
+
     return (
-        <div className="App">
-            <header className="App-header">
-            </header>
-            <h1 class="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+        <div className="container mx-auto">
+            <div className="columns-2" >
+                <div id="img-disply">
+                    <ImgSelect visible={noImg} onchange={imgChange} />
+                    <ImgDisply visible={noImg} fname={imgPath} />
+                </div>
+                <div id='option'>
+                    <h1 className="text-3xl font-bold underline">
+                        Hello world!
+                    </h1>
+                </div>
+            </div>
         </div>
     );
 }
