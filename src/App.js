e37870ec62d2269bca725dd9371dd0254c38032d
pingfury <pingfury@outlook.com>
2024-07-01 18:41:19 +0800

    fix npm to pnpm, app.js

--- src/App.js
+++ src/App.js
@@ -2,48 +2,44 @@ import { useState } from 'react';
 import './App.css';
 
 function ImgDisply({ visible, fname }) {
-    if (!visible) {
-        return <img src={fname}></img>;
-    } else {
-        return (<></>)
-    }
+  if (!visible) {
+    return <img src={fname} alt='img'></img>;
+  } else {
+    return (<></>)
+  }
 }
 
-function ImgSelect({ visible, onchange }) {
-    if (visible) {
-        return <input type='file' onChange={onchange}></input>;
-    } else {
-        return (<></>)
-    }
+function ImgSelect({ onchange }) {
+  return <input type='file' onChange={onchange}></input>;
 }
 
 function App() {
-    const [imgPath, setImgPath] = useState("");
-    const [noImg, setnoImg] = useState(true);
+  const [imgPath, setImgPath] = useState("");
+  const [noImg, setnoImg] = useState(true);
 
-    function imgChange(evt) {
-        if (evt.target.files && evt.target.files[0]) {
-            setImgPath(URL.createObjectURL(evt.target.files[0]));
-            setnoImg(false);
-        }
-        console.log(imgPath);
-    };
+  function imgChange(evt) {
+    if (evt.target.files && evt.target.files[0]) {
+      setImgPath(URL.createObjectURL(evt.target.files[0]));
+      setnoImg(false);
+    }
+    console.log(imgPath);
+  };
 
-    return (
-        <div className="container mx-auto">
-            <div className="columns-2" >
-                <div id="img-disply">
-                    <ImgSelect visible={noImg} onchange={imgChange} />
-                    <ImgDisply visible={noImg} fname={imgPath} />
-                </div>
-                <div id='option'>
-                    <h1 className="text-3xl font-bold underline">
-                        Hello world!
-                    </h1>
-                </div>
-            </div>
+  return (
+    <div className="container mx-auto">
+      <div className="flex h-screen">
+        <div id="img-disply" className='w-2/3 p-2 border border-0 rounded-lg'>
+          <ImgDisply visible={noImg} fname={imgPath} />
         </div>
-    );
+        <div id='option' className='w-1/3 p-2 border border-0 rounded-lg'>
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+          <ImgSelect onchange={imgChange} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
