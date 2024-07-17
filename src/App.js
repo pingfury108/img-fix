fc558f83ebe3372cd24360e210c7e18a269f523d
pingfury <pingfury@outlook.com>
2024-07-17 12:16:34 +0800

    fix ui

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ import './App.css';
 const ffmpeg = new FFmpeg();
 await ffmpeg.load();
 
+function secondsToTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  const milliseconds = Math.floor((seconds % 1) * 1000); // 获取毫秒部分
+
+  const formattedHours = hours.toString().padStart(2, '0');
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+  const formattedMilliseconds = milliseconds.toString().padStart(3, '0'); // 保留三位毫秒
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
+}
+
 function ImgDisply({ visible, fname }) {
   if (!visible) {
     return <img src={fname} alt='img'></img>;
@@ -14,9 +28,9 @@ function ImgDisply({ visible, fname }) {
   }
 }
 
-function VideoDisply({ visible, fname }) {
+function VideoDisply({ visible, fname, onTimeUpdateF }) {
   if (!visible) {
-    return <video controls>
+    return <video controls onTimeUpdate={onTimeUpdateF}>
       <source src={fname}></source>
     </video >;
   } else {
@@ -63,43 +77,60 @@ function App() {
 
   return (
     <div className="container mx-auto">
+      <h2 className="text-3xl font-bold text-center">
+        视频提取图片
+      </h2>
       <div className="flex h-screen w-full">
-        <div id="img-disply" className='w-2/3 p-2 border border-0 rounded-lg'>
-          <VideoDisply visible={noVideo} fname={videoData} />
+        <div id="img-disply" className='w-2/3 p-2 border border-1 rounded-lg place-content-center'>
+          <VideoDisply visible={noVideo} fname={videoData} onTimeUpdateF={e => {
+            setT(secondsToTime(e.target.currentTime))
+          }} />
         </div>
-        <div id='option' className='w-1/3 p-2 border border-0 rounded-lg'>
-          <div className='h-1/3'>
-            <h2 className="text-3xl font-bold underline">
-              视频提取图片
-            </h2>
-            <div className='container'>
-              <label>选择视频</label>
-              <ImgSelect onchange={videoChange} />
+        <div id='option' className='w-1/3 p-2 border border-1 rounded-lg place-content-center'>
+          <div className='h-1/3 border border-1 rounded-lg p-2'>
+            <div className='container grid grid-cols-4 gap-2 pt-1'>
+              <label className='col-span-1'>选择视频</label>
+              <div className='col-span-3'>
+                <ImgSelect onchange={videoChange} />
+              </div>
+            </div>
+            <div className='container grid grid-cols-4 gap-2 pt-1'>
+              <label className='col-span-1'>提取视频帧位置</label>
+              <div className='col-span-3'>
+                <input type='text' value={t} onChange={e => {
+                  setT(e.target.value)
+                }} className='border border-1 rounded-lg'></input>
+              </div>
             </div>
             <div>
-              <label>输入进度时间</label>
-              <input type='text' value={t} onChange={e => {
-                setT(e.target.value)
-              }}></input>
             </div>
-            <div>
-            </div>
-            <button
-              onClick={_ => {
-                (async () => {
-                  await ffmpeg.writeFile('input.webm', await fetchFile(videoPath));
-                  //-ss 00:01:30 -vframes:v 1 ttt_frame.png
-                  await ffmpeg.exec(['-i', 'input.webm', '-ss', `${t}`, '-vframes:v', '1', 'output.png']).catch(v => {
-                    console.log("ffmpeg exec err: ", v);
-                  });
+            <div className='container grid grid-cols-4 gap-2 pt-1'>
+              <button className='border border-1 rounded-lg p-2 col-span-1'
+                onClick={_ => {
+                  (async () => {
+                    await ffmpeg.writeFile('input.webm', await fetchFile(videoPath));
+                    //-ss 00:01:30 -vframes:v 1 ttt_frame.png
+                    await ffmpeg.exec(['-i', 'input.webm', '-ss', `${t}`, '-vframes:v', '1', 'output.png']).catch(v => {
+                      console.log("ffmpeg exec err: ", v);
+                    });
 
-                  const data = await ffmpeg.readFile('output.png');
-                  setImgPath(URL.createObjectURL(new Blob([Uint8Array.from(data)], { type: 'image/png' })));
-                  setnoImg(false);
-                })();
-              }}>提取</button>
+                    const data = await ffmpeg.readFile('output.png');
+                    setImgPath(URL.createObjectURL(new Blob([Uint8Array.from(data)], { type: 'image/png' })));
+                    setnoImg(false);
+                  })();
+                }}>提取为图片</button>
+              <button onClick={e => {
+                const link = document.createElement("a");
+                link.href = imgPath;
+                link.download = `img-fix-${t.replace(":", "_")}.png`;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                URL.revokeObjectURL(link.href);
+              }}>下载图片</button>
+            </div>
           </div>
-          <div className='h-2/3'>
+          <div className='h-2/3 border border-1 rounded-lg p-2'>
             <ImgDisply visible={noImg} fname={imgPath} />
           </div>
         </div>
